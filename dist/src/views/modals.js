0f5b10345f3f69207e9615007d9b39de9a508726
unknown <kachaksk.kh@gmail.com>
2025-09-09 21:02:05 +0700

    A7

--- dist/src/views/modals.js
+++ dist/src/views/modals.js
@@ -1,4 +1,4 @@
-import { modalContainer } from "../config/DOMElements";
+import { modalContainer } from "../config/dom-elements";
 import { postCloudData } from "../databases/google-sheets";
 import { loadCloudCardData } from "../databases/local-data";
 import { appendEvent } from "../events/events";
